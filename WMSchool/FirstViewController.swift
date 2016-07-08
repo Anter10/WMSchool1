86506ad5c14a6a2d7599f71e55aee3cfd21a207c
Anter10 <892466942@.com>
2016-07-08 14:39:05 +0800

    123
    Hellos

---
+++ WMSchool/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  WMSchool
+//
+//  Created by 郭有超 on 16/7/8.
+//  Copyright © 2016年 郭有超. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
